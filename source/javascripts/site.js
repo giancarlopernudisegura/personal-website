eeb193b4196117ea7e9315eeae266f2bf762e736
Giancarlo <pernudig@gmail.com>
2019-10-31 01:56:40 -0600

    email copy

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,8 @@
 // This is where it all goes :)
 console.log('I use Arch btw :)')
+
+function copy(textObj) {
+  textObj.select();
+  textObj.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
